b771250f68b8277c67db01181e288f21439f5e49
gaborsch <gabor.schermann@gmail.com>
2022-02-05 18:28:48 +0100

    Rocky as maven dependency

--- src/main/java/rockstar/web/RockstarHandler.java
+++ src/main/java/rockstar/web/RockstarHandler.java
@@ -25,47 +25,48 @@ import rockstar.runtime.Environment;
 
 public final class RockstarHandler extends AbstractHandler {
 	@Override
-	public void handle(String target, Request baseRequest, HttpServletRequest request,
-			HttpServletResponse response) throws IOException, ServletException {
-		
+	public void handle(String target, Request baseRequest, HttpServletRequest request, HttpServletResponse response)
+			throws IOException, ServletException {
+
 		String path = baseRequest.getPathInContext();
-		
+
 		if (path.startsWith("/execute")) {
-			System.out.println("Processor captured "+baseRequest.getPathInContext() + ", ?: "+baseRequest.getQueryString());
-			
-		    response.setContentType("text/html;charset=utf-8");
-		    response.setStatus(HttpServletResponse.SC_OK);
-		    baseRequest.setHandled(true);
-		    
-		    String source = request.getParameter("src");
-		    String input = request.getParameter("inp");		    
-		    String output = run(source, input != null ? input : "");
-		    
-		    PrintWriter writer = response.getWriter();
-		    writer.println("<h1>Output:</h1>\n<hr />\n<code>");	
-		    writer.println(output.replaceAll("\r?\n", "\n<br>\n"));
-		    writer.println("</code>");
-		    writer.println("<hr />\nDate: " +new Date());
+			System.out.println(
+					"Processor captured " + baseRequest.getPathInContext() + "?" + baseRequest.getQueryString());
+
+			response.setContentType("text/html;charset=utf-8");
+			response.setStatus(HttpServletResponse.SC_OK);
+			baseRequest.setHandled(true);
+
+			String source = request.getParameter("src");
+			String input = request.getParameter("inp");
+			String output = run(source, input != null ? input : "");
+
+			PrintWriter writer = response.getWriter();
+//			writer.println("<h1>Output:</h1>\n<hr />\n<code>");
+//			writer.println(output.replaceAll("\r?\n", "\n<br>\n"));
+//			writer.println("</code>");
+//			writer.println("<hr />\nDate: " + new Date());
+
+			writer.println(output);
+			writer.println("(Date: " + new Date() + ")");
 		} else {
-			System.out.println("Processor passed "+baseRequest.getPathInContext() + ", ?: "+baseRequest.getQueryString());
+			System.out.println(
+					"Processor passed " + baseRequest.getPathInContext() + ", ?: " + baseRequest.getQueryString());
 		}
 	}
-	
+
 	private String run(String source, String input) throws IOException {
 		RockstarApi api = new RockstarApi();
-		
+
 		CharArrayWriter outWriter = new CharArrayWriter();
 		OutputStream output = new WriterOutputStream(outWriter, StandardCharsets.UTF_8.name());
 		// error is redirected to output
 		api.setEnv(createEnvironment(input, output, output));
-		
-	    api.getEnv().getOutput().println("test ");
 
-	    api.run("-", source);
-		
-	    api.getEnv().getOutput().println("test ");
-		output.flush();
-		
+		// run Rocky
+		api.run("Rockstar", source);
+
 		return outWriter.toString();
 	}
 
@@ -75,7 +76,5 @@ public final class RockstarHandler extends AbstractHandler {
 		PrintStream epw = new PrintStream(error, true, StandardCharsets.UTF_8);
 		return Environment.create(is, opw, epw, new HashMap<String, String>());
 	}
-	
-	
-	
+
 }
